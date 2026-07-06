16d9c6a6ab7fa77afd2db86be282c7093904c358
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2026-07-06 23:08:34 +0530

    maps - iteration

--- maps/main.go
+++ maps/main.go
@@ -5,20 +5,24 @@ import "fmt"
 // all the keys are have to be a singular type -- the values too
 func main(){
 	// var colors map[string]string
+	// colors := make(map[string]string)
 
-	colors := make(map[string]string)
+	colors := map[string]string{
+		"red": "#ff0000",
+		"yellow": "#d0ee0e",
+	}
 
-	// colors := map[string]string{
-	// 	"red": "#ff0000",
-	// 	"yellow": "#d0ee0e",
-	// }
+	colors["white"] = "#fff" // cannot write colors.white cause if key is int -- it creates issues
 
-	colors["white"] = "#fff"
-	colors["red"] = "#ff0000"
+	// fmt.Println(colors)
+	// delete(colors, "white")
+	// fmt.Println(colors)
+	printMap(colors)
+}
 
-	// cannot write colors.white cause if key is int -- it creates issues
 
-	fmt.Println(colors)
-	delete(colors, "white")
-	fmt.Println(colors)
+func printMap(c map[string] string){
+	for col, hex := range c {
+		fmt.Println("Hex code for", col, "is", hex)
+	}
 }
